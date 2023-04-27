0d76b938446c97382eb37b6cfa732809f113c0f2
lefendie <lefendie@student.42.fr>
2023-04-27 21:10:18 +0200

    --wip-- [skip ci]

--- apps/website/src/components/Form.tsx
+++ apps/website/src/components/Form.tsx
@@ -1,15 +1,18 @@
-import Field from '../components/Field.tsx'
-import Button from '../components/Button.tsx'
+import Field from '../components/Field.tsx';
+import Button from '../components/Button.tsx';
 
-export default function LoginForm(option: IForm)
-{
+export default function LoginForm(option: IForm) {
 	return (
 		<div>
-		<form action ="/api/cool" method="post">
-			<Field id="login" name="login" placeholder="Enter login" size={16}/>
-			<Field id="password" name="password" placeholder="password" size={16}/>
-			<Button label="Log in"/>
-		</form>
+			<form action ={option.action
+				? option.action
+				: '/api/cool'} method={option.method
+				? option.method
+				: 'post'}>
+				<Field id="login" name="login" placeholder="Enter login" size={16}/>
+				<Field id="password" name="password" placeholder="password" size={16}/>
+				<Button type="submit" label="Log in"/>
+			</form>
 		</div>
 	);
 }
