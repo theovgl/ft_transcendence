0a07f14f220e871ff26894bfa3e96a7522f3e8ec
Leon EFENDIEV <lefendie@e2r1p12.clusters.42paris.fr>
2023-04-27 16:47:55 +0200

    --wip-- [skip ci]

---
+++ apps/website/src/components/Form.tsx
@@ -0,0 +1,15 @@
+import Field from '../components/Field.tsx'
+import Button from '../components/Button.tsx'
+
+export default function LoginForm(option: IForm)
+{
+	return (
+		<div>
+		<form action ="/api/cool" method="post">
+			<Field id="login" name="login" placeholder="Enter login" size={16}/>
+			<Field id="password" name="password" placeholder="password" size={16}/>
+			<Button label="Log in"/>
+		</form>
+		</div>
+	);
+}
