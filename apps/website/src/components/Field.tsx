d0978701b4883880ad732dc941fbd1d5e5844950
lefendie <lefendie@student.42.fr>
2023-04-27 13:24:47 +0200

    --wip-- [skip ci]

--- apps/website/src/components/Field.tsx
+++ apps/website/src/components/Field.tsx
@@ -6,15 +6,22 @@ export interface IField
 	size: number
 }
 
-export default function Field(option: IField)
-{
+export default function Field(option: IField) {
 	return (
 		<input type="text"
-		id={option.id ? option.id : "id_temp"} 
-		name={option.name ? option.name : "name_temp"} 
-		placeholder={option.placeholder ? option.placeholder : "placeholder_temp"} 
-		required
-		minLength={4} 
-		size={option.size ? option.size : 12}	/>
+			id={option.id
+				? option.id
+				: 'id_temp'} 
+			name={option.name
+				? option.name
+				: 'name_temp'} 
+			placeholder={option.placeholder
+				? option.placeholder
+				: 'placeholder_temp'} 
+			required
+			minLength={4} 
+			size={option.size
+				? option.size
+				: 12}	/>
 	);
 }
