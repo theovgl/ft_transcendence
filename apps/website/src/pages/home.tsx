26e1f41ec6a650b4869265672698c85a83840052
lefendie <lefendie@student.42.fr>
2023-04-30 10:34:34 +0200

    refactor: styles for components

--- apps/website/src/pages/home.tsx
+++ apps/website/src/pages/home.tsx
@@ -3,12 +3,16 @@ import LoginForm from '@/components/Form.tsx'
 import homepageStyle from "@/styles/homepage.module.css"
 import Navigation from "@/components/Navigation.tsx"
 import Leaderboard from "@/components/Leaderboard.tsx"
+import Button from "@/components/Button.tsx"
 
 export default function Home() {
 return (
 		<>
 				<Navigation current_page="HomePage"/>
-				<Leaderboard/>
+				<div className={homepageStyle.main}>
+					<Button style={homepageStyle.button_play} type="button" label="Play !"/>
+					<Leaderboard/>
+				</div>
 		</>
 )
 }
