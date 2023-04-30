d084c7288b3990c1d7e57ac359060552bbe63da4
lefendie <lefendie@student.42.fr>
2023-04-30 10:34:34 +0200

    refactor: styles for components

--- apps/website/src/components/Leaderboard.tsx
+++ apps/website/src/components/Leaderboard.tsx
@@ -1,16 +1,21 @@
 import homepageStyle from "@/styles/homepage.module.css"
+import leaderboardStyle from "@/styles/leaderboard.module.css"
 import Button from "@/components/Button.tsx"
 
-export interface ILeaderboard
-{
-	current_page: string,
-}
 
-
-export default function Leaderboard(option: ILeaderboard)
+export default function Leaderboard()
 {
 	return (
-		<div className={homepageStyle.leaderboard}>
+		<div className={leaderboardStyle.grid}>
+			<h1>Leaderboard</h1>
+				<div className={leaderboardStyle.main}>
+					
+				</div>
+				<div className={leaderboardStyle.button_left_grid}> <Button style={leaderboardStyle.button_left}/></div>
+				<div className={leaderboardStyle.button_right_grid}><Button style={leaderboardStyle.button_right}/></div>
+				<div className={leaderboardStyle.footer}>
+					page1/23	
+				</div>
 		</div>
 	);
 }
