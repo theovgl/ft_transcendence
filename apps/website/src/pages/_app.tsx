09e2c5369bae0950bbc42d5245c52bb91f924eb5
Théo Vogel <contact@theovogel.fr>
2023-05-01 20:01:00 +0200

    feat: add login context to the whole app

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/general.styles.css'
 import type { AppProps } from 'next/app'
+import { LoginContext } from '@/utils/contexts/LoginContext'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <LoginContext.Provider value={false}>
+      <Component {...pageProps} />
+    </LoginContext.Provider>
+  )
 }
