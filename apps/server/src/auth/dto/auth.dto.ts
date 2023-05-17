20bdc23450dc4e3a7922c94bd9d1d7b44f97e1c7
tvogel <tvogel@student.42.fr>
2023-05-17 18:36:50 +0200

    refactor: fix weird indentation behavior

--- apps/server/src/auth/dto/auth.dto.ts
+++ apps/server/src/auth/dto/auth.dto.ts
@@ -1,15 +1,15 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
-		@IsNotEmpty()
-		@IsString()
-			username: string;
+	@IsNotEmpty()
+	@IsString()
+	username: string;
 
-		@IsEmail()
-    @IsNotEmpty()
-			email: string;
+	@IsEmail()
+	@IsNotEmpty()
+	email: string;
 
-    @IsString()
-    @IsNotEmpty()
-    	password: string;
+	@IsString()
+	@IsNotEmpty()
+	password: string;
 }
--- apps/server/src/user/dto/edit-user.dto.ts
+++ apps/server/src/user/dto/edit-user.dto.ts
@@ -1,15 +1,15 @@
 import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class EditUserDto {
-    @IsEmail()
-    @IsOptional()
-    	email?:string;
+	@IsEmail()
+	@IsOptional()
+	email?:string;
 
-    @IsString()
-    @IsOptional()
-    	firstName?: string;
-    
-    @IsString()
-    @IsOptional()
-    	lastName?: string;
+	@IsString()
+	@IsOptional()
+	firstName?: string;
+
+	@IsString()
+	@IsOptional()
+	lastName?: string;
 }
