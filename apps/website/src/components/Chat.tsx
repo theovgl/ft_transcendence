af517a5dc0c926cdabde4020b2d140673b4aaae5
lefendie <lefendie@student.42.fr>
2023-05-31 21:10:25 +0200

    Refactor: Transformed Tab component into a list for better manipulation

--- apps/website/src/components/Chat.tsx
+++ apps/website/src/components/Chat.tsx
@@ -3,6 +3,7 @@ import Tab from "@/components/Tab.tsx";
 import Contact from "@/components/Contact.tsx";
 import { io, Socket } from "socket.io-client";
 import { useState, useEffect } from "react";
+import { useRouter } from 'next/router';
 
 
 type Message = {
@@ -32,23 +33,6 @@ interface SocketData {
   age: number;
 }
 
-//const tabs = Array.from(document.getElementsByClassName("ChannelTab"));
-//
-//tabs.forEach(tab => {
-//	tab.addEventListener('click', function SelectChannel()
-//	{
-//		console.log("test");
-//	});
-//});
-//
-
-type Tab = {}
-
-type GlobalProps = {
-	tab: Tab[]
-};
-
-
 
 let socket: Socket<ServerToClientEvents, ClientToServerEvents>;
 socket = io( "http://localhost:4000" );
@@ -61,6 +45,17 @@ export default function Chat()
   const [messages, setMessages] = useState<Array<Message>>([]);
   const [rooms, setRooms] = useState<Array<String>>([]);
   const [room, setRoom] = useState("");
+
+	let tabList = [	
+					{
+						label:"General",
+						active:true
+					},
+					{
+						label:"Contact 1",
+						active:false
+					}
+	];
 
 	useEffect(() => {
     	socketInitializer();
@@ -97,10 +92,11 @@ export default function Chat()
 
 	return (
 		<div className={chatStyle.grid}>
-				<div className={chatStyle.tab_list}>
-					<Tab label="General" active={true}/>
-					<Tab label="Contact 1" active={false}/>
+
+				<div className={chatStyle.tab_list}>	
+				{tabList.map(item =>  <Tab key={item.label} label={item.label} active={item.active}/>)}
 				</div>
+
 				<div className={chatStyle.contact_list}>
 					<Contact name="M.Obama" picture="temp" content="in-game" context="presentation"/>
 					<Contact name="XxX_Obama_Gaming_XxX" picture="temp" content="online" context="presentation"/>
