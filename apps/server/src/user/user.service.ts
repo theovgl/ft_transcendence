5e32bf7438a8471c268c2ed36cd6f35b267878fa
Théo Vogel <contact@theovogel.fr>
2023-05-31 19:30:29 +0200

    feat: add Nest routes to fetch user by username

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -1,11 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { EditUserDto } from './dto';
 
 @Injectable()
 export class UserService {
 	constructor(private prisma: PrismaService) {}
-    
 	async editUser(userId: number, dto: EditUserDto) {
 		const user = await this.prisma.user.update({
 			where: {
@@ -16,7 +15,25 @@ export class UserService {
 			},
 		});
 		delete user.hashedPassword;
-        
+		return user;
+	}
+
+	async findAll() {
+		const users = await this.prisma.user.findMany();
+
+		return users;
+	}
+
+	async findOneByUsername(username: string) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				name: username,
+			}
+		});
+
+		if(!user)
+			throw new NotFoundException('User: ' + username + ' does not exists');
+		delete user.hashedPassword;
 		return user;
 	}
 }
