bc3ea4330f8a8e5e802e1a68c709bdd4e6c46bc9
Théo Vogel <contact@theovogel.fr>
2023-05-31 19:30:29 +0200

    feat: add Nest routes to fetch user by username

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -1,14 +1,25 @@
-import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
-import { JwtGuard } from '../auth/guard';
-import { GetUser } from '../auth/decorator';
+import { Body, Controller, Get, Param, Patch, Req, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard'
+import { GetUser } from '../auth/decorator'
 import { User } from '@prisma/client';
 import { EditUserDto } from './dto';
 import { UserService } from './user.service';
 
-@UseGuards(JwtGuard)
+//@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
 	constructor (private userService: UserService) {}
+
+	@Get()
+	findAll() {
+		return this.userService.findAll();
+	}
+
+	@Get(':username')
+	findOneByUsername(@Param('username') username: string) {
+		console.log(username);
+		return this.userService.findOneByUsername(username);
+	}
 
 	@Get('me')
 	getMe(@GetUser() user: User, @GetUser('email') email: string){
