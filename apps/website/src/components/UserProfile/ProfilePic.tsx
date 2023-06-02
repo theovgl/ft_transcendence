ea24822dc96e605466866b18d85ddf58cef75ed3
Théo Vogel <contact@theovogel.fr>
2023-06-02 16:53:31 +0200

    feat: profile picture can now be seen on user profile page

--- apps/website/src/components/UserProfile/ProfilePic.tsx
+++ apps/website/src/components/UserProfile/ProfilePic.tsx
@@ -1,12 +1,16 @@
 import styles from '@/styles/userProfile/profilePic.module.scss';
 import Image from 'next/image';
 
-export default function ProfilePic() {
+interface Props {
+	path?: string;
+}
+
+export default function ProfilePic({path}: Props) {
 	return (
 		<Image
 			className={styles.profilePic}
 			alt='Profile icture of the user'
-			src="/test_image.jpeg"
+			src={path ? path : '/test_image.jpeg'}
 			width={90}
 			height={90}
 		/>
