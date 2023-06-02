2d78a6300cb4d020896cfef32f68f678ff73e133
Théo Vogel <contact@theovogel.fr>
2023-06-02 16:51:09 +0200

    feat: add a route in order to fetch profile pictures

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Body, Controller, Get, Param, Patch, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Param, Patch, Post, Req, Res, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard'
 import { GetUser } from '../auth/decorator'
 import { User } from '@prisma/client';
 import { EditUserDto } from './dto';
 import { UserService } from './user.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Observable, of } from 'rxjs';
 
 //@UseGuards(JwtGuard)
 @Controller('users')
@@ -47,6 +48,11 @@ export class UserController {
 
 		await this.userService.uploadProfilePicture(1, image.buffer);
 		return { message: 'Profile picture uploaded successfully' };
+	}
+
+	@Get('profile-picture/:imageName')
+	downloadProfilePicture(@Param('imageName') imageName: string, @Res() res): Observable<Object> {
+		return of(res.sendFile(process.env.UPLOADS_DESTINATION + '/profile-pictures/' + imageName));
 	}
 }
 
