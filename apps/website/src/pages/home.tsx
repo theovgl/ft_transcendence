023e044cb542d85deff134b5bfc0dbf596f6ac77
Théo Vogel <contact@theovogel.fr>
2023-06-09 19:13:44 +0200

    refactor: remove useless components

--- apps/website/src/pages/home.tsx
+++ apps/website/src/pages/home.tsx
@@ -1,13 +1,8 @@
-import Head from 'next/head';
-import LoginForm from '@/components/Form.tsx';
 import 'normalize.css';
-import homepageStyle from "@/styles/homepage.module.scss";
-import Navigation from "@/components/Navigation.tsx";
-import Leaderboard from "@/components/Leaderboard/";
-import Button from "@/components/Button/Button";
+import homepageStyle from '@/styles/homepage.module.scss';
+import Leaderboard from '@/components/Leaderboard/';
 import Navbar from '@/components/Navbar';
 import PlayButton from '@/components/PlayButton';
-import { useEffect } from 'react';
 
 export default function Home() {
 
@@ -20,13 +15,4 @@ export default function Home() {
 			</main>
 		</div>
 	);
-/* return (
-		<>
-				<Navigation current_page="HomePage"/>
-				<div className={homepageStyle.main}>
-					<Button style={homepageStyle.button_play} type="button" label="Play !"/>
-					<Leaderboard/>
-				</div>
-		</>
-)*/
 }
--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import LoginForm from '@/components/Form.tsx';
 
 export default function Home() {
 	return (
@@ -10,7 +9,6 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<LoginForm/>
 		</>
 	);
 }
