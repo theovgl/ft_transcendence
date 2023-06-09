3dced56ca9c8098981b38dc324b9d81682d067bf
ArnaudBD <arnaud.bijuduval@gmail.com>
2023-06-09 16:48:17 +0200

    feat: add email in jwt strategy validate()

--- apps/server/src/auth/strategy/jwt.strategy.ts
+++ apps/server/src/auth/strategy/jwt.strategy.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
-	constructor(config: ConfigService, private prisma: PrismaService){
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+	constructor(config: ConfigService, private prisma: PrismaService) {
 		super({
 			jwtFromRequest: 
                 ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -16,14 +16,14 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
 
 	async validate(payload: {
         sub: number;
-        // email: string;
+        email: string;
     }) {
 		const user = await this.prisma.user.findUnique({
 			where: {
 				id: payload.sub,
+				email: payload.email,
 			},
 		});
-		// delete user.hashedPassword;
 		return user;
 	}
 }
