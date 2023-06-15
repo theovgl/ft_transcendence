b9221af0a435e29e0987b99d148dfff86845c563
tvogel <tvogel@student.42.fr>
2023-06-15 15:47:36 +0200

    fix: add domain to cors policy

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -12,7 +12,10 @@ async function bootstrap() {
 	}));
 	app.use(cors(
 		{
-			origin: 'http://127.0.0.1:3000',
+			origin: [
+				'http://127.0.0.1:3000',
+				'http://localhost:3000',
+			],
 			credentials: true,
 			exposedHeaders: ['Authorization'],
 		}
