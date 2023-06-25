37082bff929f074af19105bd9027a94582cb8326
Théo Vogel <contact@theovogel.fr>
2023-06-25 18:49:26 +0200

    feat: login state is now saved on when user is successfully logged

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -2,9 +2,11 @@ import type { AppProps } from 'next/app';
 import { useAuth } from '@/utils/hooks/useAuth';
 import { AuthContext } from '@/utils/contexts/AuthContext';
 import '../styles/general.scss';
+import { useContext } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-	const { user, login, logout, setUser} = useAuth();
+	const { user, login, logout } = useAuth();
+	const { setUser } = useContext(AuthContext);
 
 	return (
 		<AuthContext.Provider value={{user, setUser}}>
