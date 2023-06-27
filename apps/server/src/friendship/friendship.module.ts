ad18ba9e20c2c9453b902aec5883eaa98fcfff23
ArnaudBD <abiju-du@student.42.fr>
2023-06-27 19:25:34 +0200

    refactor: remove unused import

--- apps/server/src/friendship/friendship.module.ts
+++ apps/server/src/friendship/friendship.module.ts
@@ -3,7 +3,6 @@ import { FriendshipController } from './friendship.controller';
 import { FriendshipService } from './friendship.service';
 
 @Module({
-	imports: [],
 	controllers: [FriendshipController],
 	providers: [FriendshipService],
 })
