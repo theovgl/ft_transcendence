484a52864c116e5c9040c672c23788c63b0a9a9e
Théo Vogel <contact@theovogel.fr>
2023-07-05 00:45:33 +0200

    fix: wrong errors on indent

--- apps/website/.eslintrc.js
+++ apps/website/.eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
 	rules: {
 		'@typescript-eslint/indent': 'off',
 		'react/react-in-jsx-scope': 'off',
+		'react/jsx-indent-props': ['error', 'tab'],
+		'react/jsx-indent': ['error', 'tab'],
 		'no-tabs': 'off',
 		'semi': [ 'error', 'always' ],
 		'indent': [ 'error', 'tab' ],
