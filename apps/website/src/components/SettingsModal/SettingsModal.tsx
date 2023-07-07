cb20492e68abd18a39fa935df5083ac1e676409f
Théo Vogel <contact@theovogel.fr>
2023-07-07 15:44:46 +0200

    refactor: simplified logic in SettingsModal components

--- apps/website/src/components/SettingsModal/SettingsModal.tsx
+++ apps/website/src/components/SettingsModal/SettingsModal.tsx
@@ -1,95 +1,85 @@
-import React, { useContext, useEffect, useState} from 'react';
+import React from 'react';
 import style from './SettingsModal.module.scss';
 import ModalLink from './ModalLink/ModalLink';
-import { BiSolidChat, BiSolidGroup, BiSolidPencil, BiSolidUser } from 'react-icons/bi';
+import {
+	BiSolidChat,
+	BiSolidExit,
+	BiSolidGroup,
+	BiSolidPencil,
+	BiSolidUser
+} from 'react-icons/bi';
 import { RxCross2 } from 'react-icons/rx';
-import { useCookies } from 'react-cookie';
-import { UserInfos } from 'global';
 import ProfilePic from '../UserProfile/ProfilePic';
-import { AuthContext } from '@/utils/contexts/AuthContext';
+import { useUser } from '@/utils/hooks/useUser';
+import { useAuth } from '@/utils/hooks/useAuth';
 
-export default function SettingsModal() {
-	const auth = useContext(AuthContext);
-	const [userInfo, setUserInfo] = useState<UserInfos | undefined>(undefined);
-	const [ cookies ] = useCookies();
-	const [ isOpen, setIsOpen ] = useState(true);
+type SettingsModalProps = {
+	isModalOpen: boolean;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-	const fetchUserInfo = async (username: string) => {
-		try {
-			await fetch(
-				`http://localhost:4000/users/${username}`, {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-						'Authorization': 'Bearer ' + cookies['jwt'],
-					},
-				})
-				.then((response) => {
-					if (!response.ok) {
-						if (response.status === 404)
-							return null;
-						throw new Error('Failed to fetch user info');
-					} else
-						return response.json();
-				})
-				.then((response: UserInfos) => {
-					setUserInfo(response);
-				});
-		} catch (error) {
-			console.error(error);
-		}
+export default function SettingsModal(props: SettingsModalProps) {
+	const { user } = useUser();
+	const { logout } = useAuth();
+	const { isModalOpen, setIsModalOpen } = props;
+
+	const handleLogout = () => {
+		setIsModalOpen(false);
+		logout();
 	};
 
-	useEffect(() => {
-		const username: string | undefined  = auth?.user?.name;
-
-		if (username && auth?.isAuthenticated)
-			fetchUserInfo(username);
-	}, []);
-	
-	if (auth?.isAuthenticated) {
-		return (
-			<>
-				<div className={`${style.overlay} ${isOpen ? '' : style.hidden}`} />
-				<div className={`${style.modal_main} ${isOpen ? '' : style.hidden}`}>
-					<div className={style.modal_header}>
-						<div className={style.modal_userInfo}>
-							<ProfilePic
-								path={userInfo?.profilePicPath}
-								size={30} stroke={false}
-							/>
-							<p className={style.modal_username}>{userInfo?.name}</p>
-						</div>
-						<RxCross2 onClick={() => setIsOpen(!isOpen)} />
+	return (
+		<>
+			<div
+				onClick={() => {
+					setIsModalOpen(false);
+				}}
+				className={`${style.overlay} ${isModalOpen ? '' : style.hidden}`}
+			/>
+			<div className={`${style.modal_main} ${isModalOpen ? '' : style.hidden}`}>
+				<div className={style.modal_header}>
+					<div className={style.modal_userInfo}>
+						<ProfilePic
+							path={user?.profilePic}
+							size={30} stroke={false}
+						/>
+						<p className={style.modal_username}>{user?.name}</p>
 					</div>
-					<div className={style.modal_links_container}>
-						<ModalLink
-							title='Your profile'
-							href={'/user/' + userInfo?.name}
-							icon={ <BiSolidUser /> }
-							onClick={() => setIsOpen(false)}
-						/>
-						<ModalLink
-							title='Edit your profile'
-							href='/edit'
-							icon={ <BiSolidPencil /> }
-							onClick={() => setIsOpen(false)}
-						/>
-						<ModalLink
-							title='Your friends'
-							href='/'
-							icon={ <BiSolidGroup /> }
-							onClick={() => setIsOpen(false)}
-						/>
-						<ModalLink
-							title='Chat'
-							href='/chat'
-							icon={<BiSolidChat />}
-							onClick={() => setIsOpen(false)}
-						/>
-					</div>
+					<RxCross2 onClick={() => setIsModalOpen(!isModalOpen)} />
 				</div>
-			</>
-		);
-	}
+				<div className={style.modal_links_container}>
+					<ModalLink
+						title='Your profile'
+						href={'/user/' + user?.name}
+						icon={ <BiSolidUser /> }
+						onClick={() => setIsModalOpen(false)}
+					/>
+					<ModalLink
+						title='Edit your profile'
+						href='/edit'
+						icon={ <BiSolidPencil /> }
+						onClick={() => setIsModalOpen(false)}
+					/>
+					<ModalLink
+						title='Your friends'
+						href='/'
+						icon={ <BiSolidGroup /> }
+						onClick={() => setIsModalOpen(false)}
+					/>
+					<ModalLink
+						title='Chat'
+						href='/chat'
+						icon={<BiSolidChat />}
+						onClick={() => setIsModalOpen(false)}
+					/>
+					<ModalLink
+						title='Logout'
+						href='/login'
+						icon={<BiSolidExit />}
+						onClick={() => handleLogout()}
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
