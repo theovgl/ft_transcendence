92af962f2eab823ca39bb38ee066030613d77686
ArnaudBD <abiju-du@student.42.fr>
2023-07-07 18:53:09 +0200

    fix: block button now correctly display when refreshing the page

--- apps/website/src/components/UserProfile/Name.tsx
+++ apps/website/src/components/UserProfile/Name.tsx
@@ -7,72 +7,64 @@ import { useCookies } from 'react-cookie';
 import { BiBlock } from 'react-icons/bi';
 
 interface IName {
-  FirstName: string;
-  LastName: string;
-  Username: string;
-}
-
-export default function Name({ FirstName, LastName, Username }: IName) {
-	const [isBlocked, setIsBlocked] = useState(false);
-	const [cookies] = useCookies();
-	const [userInfo, setUserInfo] = useState<UserInfos | undefined>(undefined);
-
-
-	useEffect(() => {
-		if (!router.isReady) return;
-		const fetchUserInfo = async () => {
+	FirstName: string;
+	LastName: string;
+	Username: string;
+	initialIsBlocked: boolean;
+	toggleBlockStatus: () => void;
+  }
+  export default function Name({ FirstName, LastName, Username, initialIsBlocked, toggleBlockStatus }: IName) {
+	  const [isBlocked, setIsBlocked] = useState(initialIsBlocked);
+	  const [cookies] = useCookies();
+	  const [userInfo, setUserInfo] = useState<UserInfos | undefined>(undefined);
+	  const [status, setStatus] = useState<string>('');
+  
+	  useEffect(() => {
+		  if (!router.isReady) return;
+		  
+		  const fetchUserInfo = async () => {
 			try {
-				await fetch(
-					`http://localhost:4000/users/${router.query.username}`, {
-						method: 'GET',
-						headers: {
-							'Content-Type': 'application/json',
-							'Authorization': 'Bearer ' + cookies['jwt'],
-						},
-					})
-					.then((response) => {
-						if (!response.ok) {
-							if (response.status === 404) {
-								router.push('/404');
-								return null;
-							}
-							throw new Error('Failed to fetch user info');
-						} else
-							return response.json();
-					})
-					.then((response: UserInfos) => {
-						setUserInfo(response);
-					});
-			} catch (error) {
-				console.error(error);
-			}
-		};
-		fetchUserInfo();
-	}, [router.isReady, router.query.username, router, cookies]);
-
-	const toggleBlock = () => {
-		let blockString = isBlocked ? 'unblock' : 'block';
-		fetch(`http://localhost:4000/friendship/${blockString}?requesterName=${encodeURIComponent(
-			jwtDecode(cookies['jwt']).username
-			)}&addresseeName=${router.query.username}`, {
+			  const statusResponse = await fetch(`http://localhost:4000/friendship/getRelationship?requesterName=${encodeURIComponent(
+				jwtDecode(cookies['jwt']).username
+			  )}&addresseeName=${router.query.username}`, {
 				method: 'GET',
 				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': 'Bearer ' + cookies['jwt'],
+				  'Content-Type': 'application/json',
+				  'Authorization': 'Bearer ' + cookies['jwt'],
 				},
+			  });
+			  
+			  const status = await statusResponse.text();
+			  if (status === 'BLOCKED') {
+				setIsBlocked(true);
+			  }
+			} catch (error) {
+			  console.error(error);
 			}
-			)
-		setIsBlocked(!isBlocked);
-		const blockIconElement = document.querySelector('.blockIcon');
-  if (blockIconElement) {
-    if (isUserBlocked) {
-      blockIconElement.classList.add('blocked');
-    } else {
-      blockIconElement.classList.remove('blocked');
-    }
-  }
-
-		};
+		  };
+		  fetchUserInfo();
+		}, [router.isReady, router.query.username, router, cookies]);
+	  const toggleBlock = async () => {
+		  let blockString = isBlocked ? 'unblock' : 'block';
+		  try {
+		  const response = await fetch(`http://localhost:4000/friendship/${blockString}?requesterName=${encodeURIComponent(
+			  jwtDecode(cookies['jwt']).username
+			  )}&addresseeName=${router.query.username}`, {
+				  method: 'GET',
+				  headers: {
+					  'Content-Type': 'application/json',
+					  'Authorization': 'Bearer ' + cookies['jwt'],
+				  },
+			  }
+			  )
+			  if(response.ok) {
+				  setIsBlocked(!isBlocked); // Toggle the isBlocked state
+				  toggleBlockStatus(); // Call the toggleBlockStatus prop function
+				}
+		  } catch (error) {
+			  console.error(error);
+		  }
+	  };
 
 	return (
 		<div className={styles.id_container}>
