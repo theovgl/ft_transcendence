07ebdd3b2653274e5af63467085c07654e02e496
Théo Vogel <contact@theovogel.fr>
2023-07-22 01:41:53 +0200

    refactor: use Jotai to manage auth state

--- apps/website/src/utils/hooks/useAuth.ts
+++ apps/website/src/utils/hooks/useAuth.ts
@@ -1,12 +1,21 @@
-import { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { useEffect, useRef } from 'react';
 import { User, useUser } from './useUser';
 import { Cookies } from 'react-cookie';
+import { atom, useAtom } from 'jotai';
+
+const isAuthenticatedAtom = atom<boolean>(false);
+const isLoadingAtom = atom<boolean>(true);
 
 export const useAuth = () => {
 	const { addUser, user } = useUser();
 	const jwtCookie = new Cookies('jwt');
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
+	const [isAuthenticated, setIsAuthenticated] = useAtom(isAuthenticatedAtom);
+	const [isLoading, setIsLoading] = useAtom(isLoadingAtom);
+	const socketRef = useRef<Socket | null>(null);
+
+	if (!socketRef.current)
+		socketRef.current = io(`http://${process.env.NEXT_PUBLIC_IP_ADDRESS}:4000`);
 
 	useEffect(() => {
 		setIsAuthenticated(user ? true : false);
