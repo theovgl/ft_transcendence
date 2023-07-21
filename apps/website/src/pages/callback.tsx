5651ef4aed04627500898a42e5db71dc939c8cfc
ppiques <ppiques@student.42.fr>
2023-07-21 14:45:16 +0200

    wip

--- apps/website/src/pages/callback.tsx
+++ apps/website/src/pages/callback.tsx
@@ -46,15 +46,44 @@ export default function CallbackPage() {
 			throw new Error('Failed to get access token: ' + error.message);
 		}
 	}
+	async function check2FA(name: string): Promise<boolean> {
+		try {
+			const twoFACheck = await fetch(
+				`http://${process.env.NEXT_PUBLIC_IP_ADDRESS}:4000/users/${name}`, {
+					method: 'GET',
+					mode: 'cors',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					credentials: 'include',
+				}
+			);
+			if (!twoFACheck.ok) {
+				const error = await twoFACheck.json();
+				throw new Error(error.message);
+			}
+			const twoFACheckResponse = await twoFACheck.json();
+			if (twoFACheckResponse.twoFA === true)
+				router.replace('/2fa');
+		} catch  (error: any) {
+			console.error(error);
+			throw new Error('Failed to get user info: ' + error.message);
+		}
+		return true;
+	}
 
 	useEffect(() => {
 		if (!router.isReady) return;
+		// const jwt = cookies.get('jwt');
+		// // if (!jwt) return;
+		// const decodedPayload: any = jwtDecode(jwt);
 
 		const code: string = router.query.code as string;
 		if (code) {
 			getAccessToken(code)
 				.then((response) => {
 					saveLoginState();
+					// check2FA(decodedPayload.username);
 					const redirectPath = response === true
 						? '/'
 						: '/login';
