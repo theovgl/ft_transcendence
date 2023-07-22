fa44593752b65f0200ef8c9468658df279b40522
tvogel <tvogel@student.42.fr>
2023-07-23 00:33:52 +0200

    feat: add message input

--- apps/website/src/components/Chat/Conversation.tsx
+++ apps/website/src/components/Chat/Conversation.tsx
@@ -1,17 +1,34 @@
+import { useForm } from 'react-hook-form';
 import { Socket } from 'socket.io-client';
+import { MessageType } from './Chat';
 import styles from './Conversation.module.scss';
 
 interface ConversationProps {
-	messages: Message[];
+	messages: MessageType[];
 	socket?: Socket | null;
+}
+
+interface UseFormInputs {
+	message: string;
 }
 
 export default function Conversation(props: ConversationProps) {
 	const { messages } = props;
+	const {
+		register,
+		handleSubmit,
+		reset
+	} = useForm<UseFormInputs>();
+
+	const onSubmit = () => {
+		console.log('Message submit');
+		reset();
+	};
 
 	return (
 		<div className={styles.conversation_container}>
 			<div className={styles.messages_container}>
+				<p>hey</p>
 				{/* {messages.map((message, i) => (
 					// <Message
 					// 	key={i}
@@ -22,9 +39,15 @@ export default function Conversation(props: ConversationProps) {
 					// />
 				))} */}
 			</div>
-			<div>
-				
-			</div>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<input
+					placeholder='Type your message...'
+					className={styles.message_input}
+					{...register('message', { required: true })}
+				/>
+			</form>
 		</div>
 	);
 }
