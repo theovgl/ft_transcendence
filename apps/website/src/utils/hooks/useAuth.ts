06883e12f790c0628e6a2c9f53bd8e6e968922d8
Théo Vogel <45829081+theovgl@users.noreply.github.com>
2023-07-22 21:01:29 +0200

    feat: use Jōtai for some state management (#44)
    * feat: edit profile page (#42)
    
    * feat: add link to settings modal
    
    * feat: reworked edit form
    
    * fix: useUser should rerender on change
    
    * refactor: change path of saved images
    
    * feat: add displayName
    
    * fix: set maximum file upload size
    
    * fix: save displayName in jwt
    
    * feat: responsive edit form
    
    * feat: user can edit his display name and profile picture using the form
    
    * refactor: user profile uses display name
    
    * fix: type had missing displayName
    
    * fix: input had wrong font-family
    
    * fix: issue when display name was already used
    
    * refactor: optimized edit form
    
    * feat: 2fa logic is mostly working
    
    * fix: checkbox is set right whether 2fa is enabled or not
    
    * refactor: remove useless imports
    
    * feat: error handling for 2fa prompt
    
    * fix: use env URL
    
    * fix: 404 error for default image
    
    * fix: default image path
    
    * refactor: remove console.log
    
    * fix: tfa is working again
    
    * fix: missing props
    
    * refactor: use Jotai to manage auth state
    
    * refactor: use removeUser function
    
    * fix: remove socket from useAuth
    
    * refactor: remove useless useLocalStorage hook

--- apps/website/src/utils/hooks/useAuth.ts
+++ apps/website/src/utils/hooks/useAuth.ts
@@ -1,18 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { User, useUser } from './useUser';
 import { Cookies } from 'react-cookie';
+import { atom, useAtom } from 'jotai';
+
+export const isAuthenticatedAtom = atom<boolean>(false);
+const isLoadingAtom = atom<boolean>(true);
 
 export const useAuth = () => {
-	const { addUser, user } = useUser();
+	const { addUser, removeUser, user } = useUser();
 	const jwtCookie = new Cookies('jwt');
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
+	const [isAuthenticated, setIsAuthenticated] = useAtom(isAuthenticatedAtom);
+	const [isLoading, setIsLoading] = useAtom(isLoadingAtom);
 
 	useEffect(() => {
-		if (user !== null)
-			setIsAuthenticated(true);
-		else
-			setIsAuthenticated(false);
+		setIsAuthenticated(user ? true : false);
 		setIsLoading(false);
 	}, [user]);
 
@@ -22,7 +23,7 @@ export const useAuth = () => {
 
 	const logout = () => {
 		setIsAuthenticated(false);
-		localStorage.removeItem('user');
+		removeUser();
 		jwtCookie.remove('jwt');
 	};
 
