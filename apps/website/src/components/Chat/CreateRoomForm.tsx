6474b9a812a3a322929ef47ff46d4b8159188f1f
tvogel <tvogel@student.42.fr>
2023-07-24 01:48:32 +0200

    feat: add switch to set private room

--- apps/website/src/components/Chat/CreateRoomForm.tsx
+++ apps/website/src/components/Chat/CreateRoomForm.tsx
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Socket } from 'socket.io-client';
 import Button from '../Button/Button';
 import FormLabel from '../EditProfile/FormLabel';
 import styles from './CreateRoomForm.module.scss';
-import { Socket } from 'socket.io-client';
 
 interface UseFormInputs {
 	roomName: string
 	password: string
+	privateToggle: string
 }
 
 interface  CreateRoomFormProps{
@@ -17,11 +19,14 @@ export default function CreateRoomForm({ socket }: CreateRoomFormProps) {
 	const {
 		register,
 		handleSubmit,
+		reset,
 		formState: { errors },
 	} = useForm<UseFormInputs>();
+	const [passwordInputType, setPasswordInputType] = useState<string>('password');
 
 	const onSubmit = (data: UseFormInputs) => {
 		console.log(data);
+		reset();
 		// socket.emit('createRoom', data.roomName, 'public')
 		// createRoom
 	};
@@ -36,6 +41,7 @@ export default function CreateRoomForm({ socket }: CreateRoomFormProps) {
 				<div className={styles.input_label_container}>
 					<FormLabel content='Room name'/>
 					<input
+						type='text'
 						className={`
 							${styles.input}
 							${errors.roomName ? styles.input_error : ''}
@@ -61,11 +67,26 @@ export default function CreateRoomForm({ socket }: CreateRoomFormProps) {
 				<div className={styles.input_label_container}>
 					<FormLabel content='Password'/>
 					<input
+						type={passwordInputType}
 						className={`
 							${styles.input}
 							${errors.password ? styles.input_error : ''}
 						`}
 						{...register('password')}/>
+					<span
+						className={styles.hidePassword}
+						onClick={() => {
+							passwordInputType === 'password' ? 
+								setPasswordInputType('text') : 
+								setPasswordInputType('password');
+						}}
+					>
+						{passwordInputType === 'password' ? 'Show password' : 'Hide password'}
+					</span>
+				</div>
+				<div className={styles.privateField_container}>
+					<span>Private ?</span>
+					<input type="checkbox" {...register('privateToggle')}/>
 				</div>
 				<Button text='Join !' type='submit'/>
 			</form>
